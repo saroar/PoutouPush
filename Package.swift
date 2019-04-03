11276b8a94361437f9dbe815e960ef4429a484b8
Alif <saroar9@gmail.com>
2019-04-03 15:39:44 +0300

    version update

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-      .package(url: "https://github.com/IBM-Swift/CCurl.git", from: "1.0.0")
+      .package(url: "https://github.com/IBM-Swift/CCurl.git", from: "1.1.0")
     ],
     targets: [
         .target(name: "PoutouPush",
